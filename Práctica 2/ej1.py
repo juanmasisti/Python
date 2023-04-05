66e88ba705dccefa1322d6d185ddbe733e9bd4af
Juan Manuel Sisti <sistijuanmanuel@gmail.com>
2023-04-05 01:42:06 -0300

    Fix archivo

--- Práctica 2/ej1.py
+++ Práctica 2/ej1.py
@@ -1,6 +1,6 @@
 import re
 
-l = re.split('[\s()]', open("README.md").read())        #Uso de expresiones regulares para cada salto de linea
+l = re.split('[\s()]', open("texto.txt").read())        #Uso de expresiones regulares para cada salto de linea
 for elem in l:                                       
     if "https" in elem: 
         print(elem)
